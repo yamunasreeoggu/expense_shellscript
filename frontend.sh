e1db01f7624912be645599594d964be5eaa817ff
yamunasreeoggu <yamunasree321@gmail.com>
2023-09-06 17:03:44 -0400

    expenses script

---
+++ frontend.sh
@@ -0,0 +1,12 @@
+dnf install nginx
+
+systemctl enable nginx
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
