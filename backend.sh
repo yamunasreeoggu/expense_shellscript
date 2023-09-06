4108913e0afef017bf104c6873eff1e125bd712c
yamunasreeoggu <yamunasree321@gmail.com>
2023-09-06 17:46:32 -0400

    expenses script

---
+++ backend.sh
@@ -0,0 +1,24 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+dnf install nodejs -y
+
+cp backend.service /etc/systemd/system/backend.service
+
+useradd expense
+
+mkdir /app
+
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+
+npm install
+
+systemctl daemon-reload
+
+systemctl enable backend
+systemctl start backend
+
+dnf install mysql -y
+
+mysql -h 172.31.28.63 -uroot -pExpenseApp@1 < /app/schema/backend.sql
