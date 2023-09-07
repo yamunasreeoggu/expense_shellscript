e4f65c896181c0fba6b2f20245a110f4d34e813d
yamunasreeoggu <yamunasree321@gmail.com>
2023-09-07 17:43:38 -0400

    Adding DNS

--- frontend.sh
+++ frontend.sh
@@ -8,7 +8,7 @@ echo Removing Nginx Default content
 rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
 
 echo Downloading Frontend code
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
 
 cd /usr/share/nginx/html
 
