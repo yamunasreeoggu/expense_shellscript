887eda0d22ff600df490e76956db482187ea62a5
yamunasreeoggu <yamunasree321@gmail.com>
2023-09-07 17:12:59 -0400

    Adding DNS

--- backend.sh
+++ backend.sh
@@ -6,6 +6,8 @@ cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
 
+rm -rf /app
+
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
