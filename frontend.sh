cf2c45ab0536ca2c96f0b922baab9c3b725e3b4d
yamunasreeoggu <yamunasree321@gmail.com>
2023-09-07 17:41:23 -0400

    Adding DNS

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,20 @@
-dnf install nginx -y
+echo Installing Nginx
+dnf install nginx -y >>/tmp/expense.log
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo Placing Expense conf file in Nginx
+cp expense.conf /etc/nginx/default.d/expense.conf >>/tmp/expense.log
 
-systemctl enable nginx
-systemctl start nginx
+echo Removing Nginx Default content
+rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
 
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo Downloading Frontend code
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
-systemctl restart nginx
+echo Extracting Frontend code
+unzip /tmp/frontend.zip >>/tmp/expense.log
+
+echo Restarting Nginx
+systemctl enable nginx >>/tmp/expense.log
+systemctl restart nginx >>/tmp/expense.log
