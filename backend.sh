729847640eb8ce47731f08bf462094dc57fe749c
yamunasreeoggu <yamunasree321@gmail.com>
2023-09-08 15:35:22 -0400

    Adding variables

--- backend.sh
+++ backend.sh
@@ -1,36 +1,38 @@
+source common.sh
+
 echo Placing NodeJS repo file
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
 
 echo Installing NOdeJS
-dnf install nodejs -y
+dnf install nodejs -y >>$log_file
 
 echo Copying backend.service file
-cp backend.service /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service >>$log_file
 
 echo Adding Application user
-useradd expense
+useradd expense >>$log_file
 
 echo Removing old app content
-rm -rf /app
+rm -rf /app >>$log_file
 mkdir /app
 
 echo Downloading backend code
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
 
 cd /app
 echo Extracting backend code
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip >>$log_file
 
 echo Installing dependencies
-npm install
+npm install >>$log_file
 
 echo Staring backend service
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+systemctl daemon-reload >>$log_file
+systemctl enable backend >>$log_file
+systemctl start backend >>$log_file
 
 echo Installing MySQL client
-dnf install mysql -y
+dnf install mysql -y >>$log_file
 
 echo Load Schema
-mysql -h mysql.yamunadevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.yamunadevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,22 @@
+source common.sh
+
 echo Installing Nginx
-dnf install nginx -y
+dnf install nginx -y >>$log_file
 
 echo Placing Expense conf file in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
 
 echo Removing Nginx default content
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$log_file
 
 echo Downloading Frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
 
 cd /usr/share/nginx/html
 
 echo Extracting Frontend code
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >>$log_file
 
 echo Restarting Nginx
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx >>$log_file
+systemctl restart nginx >>$log_file
