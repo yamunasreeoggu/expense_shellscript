d9c53c330591a3c7578b38bcccc6ff8480b89ec5
yamunasreeoggu <yamunasree321@gmail.com>
2023-09-08 13:30:37 -0400

    Adding variables

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,20 @@
-source common.sh
-
 echo Installing Nginx
-dnf install nginx -y >>$log_file
+dnf install nginx -y
 
 echo Placing Expense conf file in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 echo Removing Nginx default content
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/*
 
 echo Downloading Frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
 
 echo Extracting Frontend code
-unzip /tmp/frontend.zip >>$log_file
+unzip /tmp/frontend.zip
 
 echo Restarting Nginx
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx
+systemctl restart nginx
